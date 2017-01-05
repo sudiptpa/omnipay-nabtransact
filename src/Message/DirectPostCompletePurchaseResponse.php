20879c81f1cf8a13089659298eb60f0995e9ec77
Sujip Thapa <sudiptpa@gmail.com>
2017-01-05 10:36:13 +0545

    changed array to be sting, fixed a bug

--- src/Message/DirectPostCompletePurchaseResponse.php
+++ src/Message/DirectPostCompletePurchaseResponse.php
@@ -14,7 +14,7 @@ class DirectPostCompletePurchaseResponse extends AbstractResponse
      */
     public function isSuccessful()
     {
-        return $this->summaryCode() && in_array($this->getCode(), array(00, 08, 11));
+        return $this->summaryCode() && in_array($this->getCode(), array('00', '08', '11'));
     }
 
     public function summaryCode()
