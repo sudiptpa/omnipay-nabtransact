1aad8c57b6dc717ba153f2fca3c52029ce32b058
Sujip Thapa <sudiptpa@gmail.com>
2022-12-06 14:51:16 +0545

    Added a WebhookRequest

---
+++ src/Message/DirectPostWebhookRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Omnipay\NABTransact\Message;
+
+class DirectPostWebhookRequest extends DirectPostAbstractRequest
+{
+    private $data = [];
+
+    public function __construct($data = [])
+    {
+        $this->data = $data;
+    }
+
+    public function generateResponseFingerprint($data)
+    {
+        $hashable = [
+            $data['merchant'],
+            $data['txn_password'],
+            $data['refid'],
+            $data['amount'],
+            $data['timestamp'],
+            $data['summarycode'],
+        ];
+
+        $fields = implode('|', $hashable);
+
+        return sha1($fields);
+    }
+
+    public function vefiyFingerPrint($fingerprint)
+    {
+        $data = $this->data;
+
+        if ($fingerprint !== $this->generateResponseFingerprint($data)) {
+            $data['restext'] = $data['restext'] . ', Invalid fingerprint.';
+            $data['summarycode'] = 3;
+        }
+
+        return new DirectPostCompletePurchaseResponse($this, $data);
+    }
+
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    public function sendData($data){}
+}
