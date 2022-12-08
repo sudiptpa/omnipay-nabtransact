36082a7f7ded9a8e92155af9bb9a4bdcebb79e4d
Sujip Thapa <sudiptpa@gmail.com>
2022-12-08 15:29:20 +0545

    Added EMV 3DS Request, Response

--- src/Message/EMV3DSOrderResponse.php
+++ src/Message/EMV3DSOrderResponse.php
@@ -3,11 +3,59 @@
 namespace Omnipay\NABTransact\Message;
 
 use Omnipay\Common\Message\AbstractResponse;
+use Omnipay\Common\Message\RequestInterface;
 
 /**
  * NABTransact EMV 3DS Order Response.
  */
 class EMV3DSOrderResponse extends AbstractResponse
 {
-    //
+    /**
+     * @param RequestInterface $request
+     * @param $data
+     */
+    public function __construct(RequestInterface $request, $data)
+    {
+        parent::__construct($request, $data);
+    }
+
+    public function getResponseData($key)
+    {
+        $data = $this->data;
+
+        return isset($data[$key]) ? $data[$key] : null;
+    }
+
+    public function getOrderId()
+    {
+        return $this->getResponseData('orderId');
+    }
+
+    public function getSimpleToken()
+    {
+        return $this->getResponseData('simpleToken');
+    }
+
+    public function getOrderToken()
+    {
+        return $this->getResponseData('orderToken');
+    }
+
+    public function getProviderClientId()
+    {
+        $data = $this->data;
+
+        return isset($data['threedSecure']['providerClientId'])
+            ? $data['threedSecure']['providerClientId']
+            : null;
+    }
+
+    public function getSessionId()
+    {
+        $data = $this->data;
+
+        return isset($data['threedSecure']['sessionId'])
+            ? $data['threedSecure']['sessionId']
+            : null;
+    }
 }
