dc4c51c372dddd84f560dd9a74e20fbcd4dc611d
Sujip Thapa <sudiptpa@gmail.com>
2022-12-10 17:50:48 +0545

    Added style improvement on submit.

--- src/Message/DirectPostAuthorizeResponse.php
+++ src/Message/DirectPostAuthorizeResponse.php
@@ -2,9 +2,12 @@
 
 namespace Omnipay\NABTransact\Message;
 
+use Omnipay\Common\Exception\RuntimeException;
 use Omnipay\Common\Message\AbstractResponse;
 use Omnipay\Common\Message\RedirectResponseInterface;
 use Omnipay\Common\Message\RequestInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse as HttpRedirectResponse;
+use Symfony\Component\HttpFoundation\Response as HttpResponse;
 
 /**
  * NABTransact Direct Post Authorize Response.
@@ -15,7 +18,7 @@ class DirectPostAuthorizeResponse extends AbstractResponse implements RedirectRe
 
     /**
      * @param RequestInterface $request
-     * @param $data
+     * @param array $data
      * @param $redirectUrl
      */
     public function __construct(RequestInterface $request, $data, $redirectUrl)
@@ -48,5 +51,98 @@ class DirectPostAuthorizeResponse extends AbstractResponse implements RedirectRe
     public function getRedirectData()
     {
         return $this->getData();
+    }
+
+    /**
+     * @return HttpRedirectResponse
+     */
+    public function getRedirectResponse()
+    {
+        if (!$this instanceof RedirectResponseInterface || !$this->isRedirect()) {
+            throw new RuntimeException('This response does not support redirection.');
+        }
+
+        if ('GET' === $this->getRedirectMethod()) {
+            return HttpRedirectResponse::create($this->getRedirectUrl());
+        } elseif ('POST' === $this->getRedirectMethod()) {
+            $hiddenFields = '';
+            foreach ($this->getRedirectData() as $key => $value) {
+                $hiddenFields .= sprintf(
+                    '<input type="hidden" name="%1$s" value="%2$s" />',
+                    htmlentities($key, ENT_QUOTES, 'UTF-8', false),
+                    htmlentities($value, ENT_QUOTES, 'UTF-8', false)
+                ) . "\n";
+            }
+
+            $style = '<style>
+                * {
+                    box-sizing: border-box;
+                }
+
+                body {
+                    font-family: Arial, Helvetica, sans-serif;
+                }
+
+                .container {
+                    width: 100%;
+                    margin-top: 20%;
+                    text-align: center;
+                    display: table;
+                }
+
+                .loader {
+                    border: 5px solid #f3f3f3;
+                    border-radius: 50%;
+                    border-top: 5px solid #3498db;
+                    width: 50px;
+                    height: 50px;
+                    display: inline-block;
+                    -webkit-animation: spin 2s linear infinite;
+                    animation: spin 2s linear infinite;
+                }
+
+                @-webkit-keyframes spin {
+                    0% { -webkit-transform: rotate(0deg); }
+                    100% { -webkit-transform: rotate(360deg); }
+                }
+
+                @keyframes spin {
+                    0% { transform: rotate(0deg); }
+                    100% { transform: rotate(360deg); }
+                }
+            </style>';
+
+            $output = '<!DOCTYPE html>
+                        <html>
+                            <head>
+                                <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+                                <title>Redirecting...</title>
+                                HEAD_STYLE
+                            </head>
+                            <body onload="document.forms[0].submit();">
+                                <form action="%1$s" method="post">
+                                    <p>Redirecting to payment page...</p>
+                                    <div>
+                                        %2$s
+                                    </div>
+                                </form>
+                                <div class="container">
+                                    <div class="loader"></div>
+                                    <h4>Processing your payment ...</h4>
+                                </div>
+                            </body>
+                        </html>';
+
+            $output = vsprintf($output, [
+                htmlentities($this->getRedirectUrl(), ENT_QUOTES, 'UTF-8', false),
+                $hiddenFields,
+            ]);
+
+            $output = str_replace('HEAD_STYLE', $style, $output);
+
+            return HttpResponse::create($output);
+        }
+
+        throw new RuntimeException('Invalid redirect method "' . $this->getRedirectMethod() . '".');
     }
 }
